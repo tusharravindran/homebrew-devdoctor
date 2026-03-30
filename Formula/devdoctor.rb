139f02777240985268c7d5001c8891aa4d782f45
tushar R <ext.tushar@kula.ai>
2026-03-30 16:38:00 +0530

    devdoctor 1.1.0

--- Formula/devdoctor.rb
+++ Formula/devdoctor.rb
@@ -3,8 +3,8 @@ class Devdoctor < Formula
 
   desc "Real-time log diagnostics CLI for backend developers"
   homepage "https://github.com/tusharravindran/devdoctor"
-  url "https://github.com/tusharravindran/devdoctor/archive/refs/tags/v1.0.7.tar.gz"
-  sha256 "5d79b2e8a3834198534a16aa912781e153dd322ade07760104c01153e9a81394"
+  url "https://github.com/tusharravindran/devdoctor/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "955f9781a66d1cd38b636d26a1ad7b3435b374100231730861ac7261b5b5b9d0"
   license "MIT"
 
   depends_on "python@3.13"
@@ -14,7 +14,7 @@ class Devdoctor < Formula
   end
 
   test do
-    assert_match "devdoctor 1.0.7", shell_output("#{bin}/devdoctor --version")
+    assert_match "devdoctor 1.1.0", shell_output("#{bin}/devdoctor --version")
     assert_match "run", shell_output("#{bin}/devdoctor --help")
     assert_match "watch", shell_output("#{bin}/devdoctor --help")
   end
